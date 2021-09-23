5447555955d2d6072a2f37f8206ad1cec7f0e641
shivraj2806 <shivjadhav2806@gmail.com>
2021-09-23 15:17:10 +0530

    Added: Printmax generic method

--- src/com/Bridgelabz/FindMaximum.java
+++ src/com/Bridgelabz/FindMaximum.java
@@ -1,4 +1,6 @@
 package com.Bridgelabz;
+ //UC5 Write the printmax generic method
+
 public class FindMaximum<E extends Comparable<E>> {
 
     E firstValue;
@@ -31,17 +33,20 @@ public class FindMaximum<E extends Comparable<E>> {
 
     public <E extends Comparable<E>> E getMax() {
         E max = (E) getMax(firstValue, secondValue, thirdValue, extraParamaters);
+        printMax(max);
         return max;
     }
 
+    public <E extends Comparable<E>> void printMax(E max) {
+        System.out.println("Maximum Value " + max);
+    }
+
+
     public static void main(String[] args) {
 
-        System.out.println("Maximum Value Of Integer:");
-        System.out.println(new FindMaximum(40, 10, 90, 100,200).getMax());
-        System.out.println("Maximum Value Of Flot:");
-        System.out.println(new FindMaximum(1.2f, 4.2f, 5.6f, 8.5f).getMax());
-        System.out.println("Maximum Value Of String:");
-        System.out.println(new FindMaximum("apple", "banana", "peach", "grapes").getMax());
+        new FindMaximum(40, 10, 90, 100).getMax();
+        new FindMaximum(1.2f, 4.2f, 5.6f, 8.5f).getMax();
+        new FindMaximum("apple", "banana", "peach", "grapes").getMax();
 
     }
 }
