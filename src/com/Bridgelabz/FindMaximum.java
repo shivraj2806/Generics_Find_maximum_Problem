87d1f68c497a575072328e933d33a50c9ccb655a
shivraj2806 <shivjadhav2806@gmail.com>
2021-09-23 13:49:41 +0530

    Added: To find maximum float value

--- src/com/Bridgelabz/FindMaximum.java
+++ src/com/Bridgelabz/FindMaximum.java
@@ -10,12 +10,21 @@ public class FindMaximum {
 
         return max;
     }
+    public Float getMax(Float firstValue, Float secondValue, Float thirdValue) {
+        Float max = firstValue;
+        if (secondValue.compareTo(max) > 0)
+            max = secondValue;
+        if (thirdValue.compareTo(max) > 0)
+            max = thirdValue;
+
+        return max;
+    }
 
     public static void main(String[] args) {
 
         FindMaximum maximum = new FindMaximum();
         System.out.println("Maximum Value:");
-        System.out.println(maximum.getMax(60, 50, 55));
+        System.out.println(maximum.getMax(12.3f, 15.8f, 85.3f));
 
     }
 }
