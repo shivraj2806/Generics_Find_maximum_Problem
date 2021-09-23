dad2234053a22568950c6c05091f17fc87e29753
shivraj2806 <shivjadhav2806@gmail.com>
2021-09-23 14:56:07 +0530

    Added: Refactor generic class for maximum value

--- src/com/Bridgelabz/FindMaximum.java
+++ src/com/Bridgelabz/FindMaximum.java
@@ -1,43 +1,43 @@
 package com.Bridgelabz;
+ //Refactor to create Generic class to take in three variables of generic type
 
-public class FindMaximum {
-    public Integer getMax(Integer firstValue, Integer secondValue, Integer thirdValue) {
-        Integer max = firstValue;
-        if (secondValue.compareTo(max) > 0)
-            max = secondValue;
-        if (thirdValue.compareTo(max) > 0)
-            max = thirdValue;
+public class FindMaximum<E extends Comparable<E>> {
 
-        return max;
+        E firstValue;
+        E secondValue;
+        E thirdValue;
+
+        public FindMaximum(E firstValue, E secondValue, E thirdValue) {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+            this.thirdValue = thirdValue;
+        }
+
+        public static <E extends Comparable<E>> E getMax(E firstValue, E secondValue, E thirdValue) {
+            E max = firstValue;
+            if (secondValue.compareTo(max) > 0)
+                max = secondValue;
+            if (thirdValue.compareTo(max) > 0)
+                max = thirdValue;
+            return max;
+        }
+
+        public <E extends Comparable<E>> E getMax() {
+            E max = (E) getMax(firstValue, secondValue, thirdValue);
+            return max;
+        }
+
+        public static void main(String[] args) {
+            System.out.println("Maximum Value Of Integer:");
+            System.out.println(new FindMaximum(40, 10, 90).getMax());
+            System.out.println("Maximum Value Of Flot:");
+            System.out.println(new FindMaximum(1.2f, 4.2f, 5.6f).getMax());
+            System.out.println("Maximum Value Of String:");
+            System.out.println(new FindMaximum("apple", "banana", "peach").getMax());
+        }
     }
-    public Float getMax(Float firstValue, Float secondValue, Float thirdValue) {
-        Float max = firstValue;
-        if (secondValue.compareTo(max) > 0)
-            max = secondValue;
-        if (thirdValue.compareTo(max) > 0)
-            max = thirdValue;
 
-        return max;
 
-    }
-    public String getMax(String firstValue, String secondValue, String thirdValue) {
-        String max = firstValue;
-        if (secondValue.compareTo(max) > 0)
-            max = secondValue;
-        if (thirdValue.compareTo(max) > 0)
-            max = thirdValue;
-
-        return max;
-    }
-
-    public static void main(String[] args) {
-
-        FindMaximum maximum = new FindMaximum();
-        System.out.println("Maximum Value:");
-        System.out.println(maximum.getMax("Apple", "Peach", "Banana"));
-
-    }
-}
 
 
 
