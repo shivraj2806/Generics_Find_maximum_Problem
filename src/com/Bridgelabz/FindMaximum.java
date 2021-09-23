748df13c22e08f33c14cd5982a504345856e78a8
shivraj2806 <shivjadhav2806@gmail.com>
2021-09-23 13:14:16 +0530

    Added: To find maximum integer value

--- src/com/Bridgelabz/FindMaximum.java
+++ src/com/Bridgelabz/FindMaximum.java
@@ -1,8 +1,22 @@
 package com.Bridgelabz;
 
 public class FindMaximum {
+    public Integer getMax(Integer firstValue, Integer secondValue, Integer thirdValue) {
+        Integer max = firstValue;
+        if (secondValue.compareTo(max) > 0)
+            max = secondValue;
+        if (thirdValue.compareTo(max) > 0)
+            max = thirdValue;
+
+        return max;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome message");
+
+        FindMaximum maximum = new FindMaximum();
+        System.out.println("Maximum Value:");
+        System.out.println(maximum.getMax(60, 50, 55));
+
     }
 }
 
